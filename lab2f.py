ca8e249baf7a2b6af409b15ce01ece1d7c6b6b67
Jonathan.Hopkins <jshopkins@myseneca.ca>
2025-01-20 10:56:04 -0500

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+##Author: Jonathan Hopkins
+##Author ID: 013095971
+##Date Created: 2025/01/20
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
