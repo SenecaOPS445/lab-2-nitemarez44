364a5254728c59a5db8230cae77746fb0acb34bc
Jonathan.Hopkins <jshopkins@myseneca.ca>
2025-01-20 10:55:40 -0500

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+##Author: Jonathan Hopkins
+##Author ID: 013095971
+##Date Created: 2025/01/20
+
+count = 10
+
+while count != 0:
+    print(count)
+    count = count - 1
+print('blast off!')
